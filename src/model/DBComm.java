387117767d40ced34ed3f4a184c14a032a4c09d7
Tyler Williams <tylerwilliams3000@gmail.com>
2014-12-11 11:31:15 -0600

    Remove some unused imports

--- src/model/DBComm.java
+++ src/model/DBComm.java
@@ -2,10 +2,8 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import ui.SettingsPanel;
 
